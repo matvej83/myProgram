5fe82247af2a01980b330258e23c5d3dad5c4581
matvej83 <55102679+matvej83@users.noreply.github.com>
2019-09-29 11:39:31 +0300

    Add files via upload
    Change file Group

--- Group.java
+++ Group.java
@@ -1,8 +1,10 @@
 package groupOfStudents;
 
-public class Group {
-    public static void main(String[] args) {
+import java.util.*;
 
+abstract public class Group {
+
+    public static Student[] formAGroup() {
         Student[] student = new Student[10];
         student[0] = new Student("Andrew", 20);
         student[1] = new ContractStudent("Anna", 19, 15000d);
@@ -14,11 +16,24 @@ public class Group {
         student[7] = new Student("Natalie", 19);
         student[8] = new Student("Bill", 21);
         student[9] = new Student("Jacob", 20);
+        return student;
+    }
+
+    public static ContractStudent[] selectContractStudent(Student[] student) {
+        int counter = 0;
         for (int i = 0; i < student.length; i++) {
             if (student[i] instanceof ContractStudent) {
-                student[i].display();
+                counter++;
             }
         }
-
+        ContractStudent[] contractStudent = new ContractStudent[counter];
+        int j = 0;
+        for (int i = 0; i < student.length; i++) {
+            if (student[i] instanceof ContractStudent) {
+                contractStudent[j] = (ContractStudent) student[i];
+                j++;
+            }
+        }
+        return contractStudent;
     }
 }
