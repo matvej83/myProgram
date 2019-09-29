156404bb65a7d0963db99bf545b9e85389c0c11a
matvej83 <55102679+matvej83@users.noreply.github.com>
2019-09-29 11:40:07 +0300

    Add files via upload
    Change file Student

--- Student.java
+++ Student.java
@@ -1,6 +1,6 @@
 package groupOfStudents;
 
-public class Student {
+public class Student extends Group {
     private String name;
     private int age;
 
@@ -13,19 +13,8 @@ public class Student {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void display() {
-        System.out.println("Student name: " + this.getName() + " Age: " + this.getAge());
-    }
 }
