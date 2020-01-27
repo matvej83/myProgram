77fadaa0e4b4dc7896d20a0363e0958d23856b0d
matvej83 <matvej83@gmail.com>
2020-01-27 22:02:08 +0200

    Bugs fixed in the task jsproject

--- jsproject/index.js
+++ jsproject/index.js
@@ -1,8 +1,11 @@
-﻿//task#0
+﻿//JavaScript
+//task#0. Write java script, that asks name in pop-up window and, if you press <OK> button, display entered name in
+// another pop-up window. Else, if you press <Escape> button - display message 'Could not show name!'.
 let name = prompt('Enter your name: ', name);
 confirm('New window has created!') ? alert(name) : alert('Could not show name!');
 
-//task#1
+//task#1. Form array with name 'group', that contains students personal data (f.e.: name, last name, age etc.). Write
+//function that output array 'group' in console.
 var group = [
 
     {
@@ -37,7 +40,7 @@ function getStudentList(arrayOfStudent) {
 
 getStudentList(group);
 
-//task#2
+//task#2. Write function, that allow to add new students in array 'group', described above.
 function addNewStudent(arrayOfStudent, name, lastName, age, laptop, gender) {
     arrayOfStudent.push({name, lastName, age, laptop, gender});
 }
@@ -45,7 +48,7 @@ function addNewStudent(arrayOfStudent, name, lastName, age, laptop, gender) {
 addNewStudent(group, "Michael", "Jordan", 23, true, "male");
 getStudentList(group);
 
-//task#3
+//task#3. Write function, that deletes symbols with code > 1103 and < 1040 from given string.
 function parseString(inputString) {
     let outputString = "";
     for (let i = 0; i < inputString.length; i++) {
@@ -61,8 +64,8 @@ console.log(initialString);
 let finalString = parseString(initialString);
 console.log(finalString);
 
-//task#4
-initialArray = [1, 5, 28, 365, 78, 15];
+//task#4. Write function for array sorting without for-loop using.
+let initialArray = [1, 5, 28, 365, 78, 15];
 console.log(initialArray);
 
 var mergeSort = function (array) {
@@ -98,12 +101,12 @@ var mergeSort = function (array) {
 
     return mergeAndSort(array);
 };
-sortedArray = mergeSort(initialArray);
+let sortedArray = mergeSort(initialArray);
 console.log(sortedArray);
 
-//task5
-names = ["Adam", "Nick", "Alicia"];
-lastNames = ["Smith", "Simpson", "Brown"];
+//task5. From two arrays: first - with names and second - with last names, form common array with full names.
+let names = ["Adam", "Nick", "Alicia"];
+let lastNames = ["Smith", "Simpson", "Brown"];
 let newArray = [];
 for (let i = 0; i < names.length; i++) {
     newArray.push(names[i] + " " + lastNames[i]);
